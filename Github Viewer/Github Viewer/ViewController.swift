1879333eeb2746fe574d89d51fdf8b0299420888
Dohyun Kim <rlaehgus1229@gmail.com>
2019-08-23 20:01:34 +0900

    File Update

--- Github Viewer/Github Viewer/ViewController.swift
+++ Github Viewer/Github Viewer/ViewController.swift
@@ -9,17 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    let url = "https://api.github.com"
     
-   
+    
     override func viewDidLoad() {
+        getData()
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
-
-}
-class repo: UITableViewCell {
+    func getData() {
+        var request = URLRequest(url: URL(string: "https://api.github.com")!)
+         request.addValue("2f3f62e9847d43410ffbf76fbd5633ea49fed7e5", forHTTPHeaderField: "")
+        request.httpMethod = "GET"
+        URLSession.shared.dataTask(with: request){
+            [weak self] data, res, err in
+            guard self != nil else { return }
+            if let err = err { print(err.localizedDescription); return }
+            print((res as! HTTPURLResponse).statusCode)
+            switch (res as! HTTPURLResponse).statusCode{
+            case 200:
+                let jsonSerialization = try! JSONSerialization.jsonObject(with: data!, options: []) as! [String:Any]
+                print(jsonSerialization)
+                print(jsonSerialization["current_user_url"]!)
+            default:
+                print("error")
+            }
+            }.resume()
+    }
+    class repo: UITableViewCell {
+        
+    }
     
 }
