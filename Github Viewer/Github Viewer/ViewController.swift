68c59c08abb01bd6fbe78f420c358cf3a5a1b353
Dohyun Kim <rlaehgus1229@gmail.com>
2019-09-01 00:44:22 +0900

    File Update

--- Github Viewer/Github Viewer/ViewController.swift
+++ Github Viewer/Github Viewer/ViewController.swift
@@ -11,11 +11,12 @@ import UIKit
 class ViewController: UITableViewController {
     
     var list = [repo]()
-   
+
+    
     
     func getData() {
         var request = URLRequest(url: URL(string: "https://api.github.com")!)
-         request.addValue("2f3f62e9847d43410ffbf76fbd5633ea49fed7e5", forHTTPHeaderField: "")
+        request.addValue("2f3f62e9847d43410ffbf76fbd5633ea49fed7e5", forHTTPHeaderField: "")
         request.httpMethod = "GET"
         URLSession.shared.dataTask(with: request){
             [weak self] data, res, err in
@@ -32,23 +33,31 @@ class ViewController: UITableViewController {
             }
             }.resume()
     }
-   
-   
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection: Int) -> Int {
+        return self.list.count
     }
     
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let row = self.list[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell")!
+        cell.textLabel?.text = "\(JSONSerialization.self)"
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        NSLog("\(indexPath.row)")
+    }
+}
+
 
 
 
 class repo: UITableViewCell {
     var repoName : String?
 }
-class repoViewController: UITableViewController {
-    
-    
-    override func viewDidLoad() {
-        var rpo = repo()
-        rpo.repoName = "\(JSONSerialization.self)"
-        
-}
 
-}
+
+
